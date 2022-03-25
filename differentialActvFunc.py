4a25af2b852527f7bf4ecc56241fd88857696bf6
Allief Nuriman <57051116+alliefn@users.noreply.github.com>
2022-03-25 21:34:49 +0700

    Add comments

--- differentialActvFunc.py
+++ differentialActvFunc.py
@@ -31,21 +31,38 @@ def difflossFuncSumofSquaredErrors(y, y_hat):
     return y - y_hat
 
 # lossfunction for softmax: E = -log(p_c)
-def lossFuncCrossEntropy(p, j, c):
+def lossFuncCrossEntropy(p, j):
+    # [DESC]
+    # Lossfunction for softmax
+    # p is the probability
+    # j is the neuron index
     return -log(p[j])
 
 # derivative of lossfunction for softmax
-def diffLossFuncCrossEntropy(p, j, c):
+def diffLossFuncCrossEntropy(p, j):
+    # [DESC]
+    # Derivative of lossfunction for softmax
+    # p is the probability
+    # j is the neuron index
     return -(1/(p[j]*ln(10)))
 
 def outputActivationFunc(t,o,x,j,i):
     # [DESC]
     # Neuron output activation function other than softmax
-
+    # t is the target
+    # o is the output
+    # x is the input
+    # j is the neuron index
+    # i is the input index
     return -(t[j] - o[j])*o[j]*(1-o[j])*x[j][i]
 
 def outputActivationFuncSoftmax(p, j, c, x, i):
     # [DESC]
     # Softmax output activation function
+    # p is the probability
+    # j is the neuron index
+    # c is the target class
+    # x is the input
+    # i is the input index
 
     return diffSoftmaxActvFunc(p, j, c) * x[j][i]
